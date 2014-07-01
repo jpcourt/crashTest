cab6a3d94c5a5d8112bf29abb8f1c74f7ae55876
unknown <jpcourt@FRLUMLPT0016840.cpgrp.root.local>
2014-07-01 10:17:26 +0200

    Ajout d'une variable globale et d'un log

--- crashTestLib.js
+++ crashTestLib.js
@@ -1,9 +1,13 @@
+var theResponse;
+
 function getJSONFile(){
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET','jsonTestFile.json');
 	xhr.onreadystatechange = function(aEvt) {
 		if (xhr.readyState == 4) {
 			if (xhr.status == 200) {
+				theResponse = xhr.response;
+				console.log(xhr.response);
 				document.getElementById('renderZone').innerHTML = JSON.responseText;
 			}
 		}
