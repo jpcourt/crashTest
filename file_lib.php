2216a18f7b45e00babe2060f2fddcb486d135d4c
unknown <jpcourt@FRLUMLPT0016840.cpgrp.root.local>
2014-07-02 18:55:35 +0200

    modification de la fonction read_file en utilisant fread

--- file_lib.php
+++ file_lib.php
@@ -22,12 +22,7 @@
 	function read_file($filepath){
 		//Lit un fichier présent en local
 		$handle = fopen($filepath,'r');
-		$data;
-		if($handle){
-			while(($buffer = fgets($handle)) !== false){
-				$data .= $buffer;
-			}
-		}		
+		$data = fread($handle, filesize($filepath));
 		fclose($handle);
 		unset($handle);
 		return $data;
