d6d47f4d89e18622b62520b01221aa1212e09735
unknown <jpcourt@FRLUMLPT0016840.cpgrp.root.local>
2014-07-09 14:37:48 +0200

    Correction de bug : dans la fonction create table et dans echo

--- sqlite_test3.php
+++ sqlite_test3.php
@@ -7,7 +7,7 @@ $size = $argv[2];
 
 $db = new dBase($db_name.'.sqlite', './data');
 
-$q = $db->exec('CREATE TABLE table1 (key1 INT, key2 INT, key3 INT, PRIMARY KEY (key1)');
+$q = $db->exec('CREATE TABLE IF NOT EXISTS table1 (key1 INT, key2 INT, key3 INT, PRIMARY KEY (key1))');
 
 for($i = 0; $i < $size; $i++){
 	$q = $db->exec('INSERT INTO table1 (key1, key2, key3) VALUES ('.$i.', '.$i.', '.$i.')');
@@ -19,7 +19,7 @@ echo "Debut du test\r\n";
 
 $result = sql_query($db, $query);
 
-echo "Lecture de la base :".$db." de taille ".$size."\r\n";
+echo "Lecture de la base :".$db_name." de taille ".$size."\r\n";
 
 if(is_array($result)){
 	print_r($result);
