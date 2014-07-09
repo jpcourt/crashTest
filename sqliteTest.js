66bade63899db94e31e438bff2dbe86f59fb9894
unknown <jpcourt@FRLUMLPT0016840.cpgrp.root.local>
2014-07-09 12:12:03 +0200

    Ajout HTML/JS pour test SQLITE

---
+++ sqliteTest.js
@@ -0,0 +1,13 @@
+function callSqliteTest(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'sqlite_test.php');
+	xhr.onreadystatechange = function(aEvt) {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				console.log(xhr.response);
+				document.getElementById('body').innerHTML = xhr.response;
+			}
+		}
+	}
+	xhr.send();	
+}
